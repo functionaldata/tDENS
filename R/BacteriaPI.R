af2df47fa6248ff0d037be228391fde9fded9e58
hadjipantelis <p.z.hadjipantelis@gmail.com>
2017-09-12 21:39:14 +0100

    Clean-up

---
+++ R/BacteriaPI.R
@@ -0,0 +1,15 @@
+#' pH distribution of 813 bacterial organisms
+#' 
+#' The approximate kernel density estimates of the 813 bacterial 
+#' organisms' isoelectric point (pI) protein distributions.
+#'
+#' @name BacteriaPI
+#' @docType data
+#' @format A matrix with 813 rows and 768 columns:
+#' \describe{
+#' \item{rowname}{General organism identifier}
+#' \item{colspace}{pH in [0,14]}
+#' }  
+#' @references
+#' The authors would like to thank Dr. Michael Knight for providing the original data
+NULL
