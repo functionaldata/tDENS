accc2cc8acf1ba9dc5622d3b5247539111b36bd5
alexpete <alxpetersen@gmail.com>
2017-10-03 16:05:49 -0700

    Minor change

--- R/GetFVE.R
+++ R/GetFVE.R
@@ -21,7 +21,8 @@
 #' # Compute FVE - must compare to regularized densities 
 #' 
 #' fve.L2 = GetFVE(fpcaObj = X, dmatrix = t(Top50BabyNames$dens$male), dSup = dSup, useAlpha = TRUE)
-#' fve.W = GetFVE(fpcaObj = X, dmatrix = t(Top50BabyNames$dens$male), dSup = dSup, metric = 'W', useAlpha = TRUE)
+#' fve.W = GetFVE(fpcaObj = X, dmatrix = t(Top50BabyNames$dens$male), dSup = dSup, 
+#'                    metric = 'W', useAlpha = TRUE)
 #' 
 #' @seealso \code{\link{RegulariseByAlpha},\link{lqd2quantile}}
 #' 
